b56bfbec01a02b9b2efa293d7a656336ae339101
Wesley Romualdo da Silva <wesley.romualdo@fde.sp.gov.br>
2024-01-23 18:10:13 -0300

    ok

--- src/routers/route_automacao.py
+++ src/routers/route_automacao.py
@@ -142,7 +142,7 @@ async def download_worker(automacao_id: int,db: Session = Depends(get_db), usuar
         
         config = dotenv_values(".env")
         config = json.loads((json.dumps(config) ))
-        s3_client = boto3.client('s3', aws_access_key_id=config['AWS_ACCESS_KEY_ID'], aws_secret_access_key=config['AWS_SECRET_ACCESS_KEY'])
+        #s3_client = boto3.client('s3', aws_access_key_id=config['AWS_ACCESS_KEY_ID'], aws_secret_access_key=config['AWS_SECRET_ACCESS_KEY'])
 
         response = requests.get(f"169.254.170.2{os.getenv('AWS_CONTAINER_CREDENTIALS_RELATIVE_URI')}")
         #/v2/credentials/65b80715-ac9d-4752-88b3-b927bc830a6f
